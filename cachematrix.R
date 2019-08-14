78dae3847bab2176c50e5aa708e943dced16d486
StewODell <40680047+StewODell@users.noreply.github.com>
2019-08-14 13:41:29 -0500

    Add files via upload
    R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## sets matrix value, clears inverse cache; gets matrix and inverse values; sets inverse value from cacheSolve 
 
 makeCacheMatrix <- function(x = matrix()) {
+  set <- function(y) {
+    mat <<- y
+    minv <<- NULL
+  }    
+  get <- function() mat    
+  setinv <- function(invrs) minv <<- invrs
+  getinv <- function() minv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## solves for inverse if none cached; returns cached inverse if cache value exists
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  minv <- x$getinv()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$setinv(minv)
+  minv
+  }
